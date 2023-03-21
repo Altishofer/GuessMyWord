7d23bfd4fc6f36e2470531b15f4aa373f8be6ba1
sandrin <sandrin@hunkeler.ws>
2023-03-22 00:05:19 +0100

    added ramdom word selection from corpora

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,26 @@
-import React, {useEffect, useState} from 'react';
-import './styles.css';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
 
-const wordTypes = ['Noun', 'Verb', 'Adjective'];
+const wordTypes = ["noun", "verb", "adjective", "adverb", "preposition"];
+const cefrLevels = ["A1", "A2", "B1", "B2", "C1", "C2"];
 
 function Index() {
+    const [hintIndex, setHintIndex] = useState(0);
+    const [userInput, setUserInput] = useState("");
     const [wordType, setWordType] = useState(wordTypes[0]);
-    const [word, setWord] = useState('');
-    const [hints, setHints] = useState(['Hint 1']);
+    const [cefrLevel, setCefrLevel] = useState(cefrLevels[0]);
+    const [word, setWord] = useState("");
+    const [hints, setHints] = useState(["Hint 1"]);
+    const [activatedWordTypes, setActivatedWordTypes] = useState(
+        wordTypes.map(() => false)
+    );
+    const [activatedCefrLevels, setActivatedCefrLevels] = useState(
+        cefrLevels.map(() => false)
+    );
 
     useEffect(() => {
         var container = document.querySelector(".container");
-        if (word === str) {
+        if (word === "dog") {
             container.classList.add("wiggle");
         } else {
             container.classList.remove("wiggle");
@@ -18,7 +28,19 @@ function Index() {
     }, [word]);
 
     const handleWordTypeChange = (type) => {
+        const newActivatedWordTypes = [...activatedWordTypes];
+        newActivatedWordTypes[wordTypes.indexOf(type)] =
+            !activatedWordTypes[wordTypes.indexOf(type)];
+        setActivatedWordTypes(newActivatedWordTypes);
         setWordType(type);
+    };
+
+    const handleCefrLevelChange = (level) => {
+        const newActivatedCefrLevels = [...activatedCefrLevels];
+        newActivatedCefrLevels[cefrLevels.indexOf(level)] =
+            !activatedCefrLevels[cefrLevels.indexOf(level)];
+        setActivatedCefrLevels(newActivatedCefrLevels);
+        setCefrLevel(level);
     };
 
     const handleWordChange = (event) => {
@@ -29,17 +51,44 @@ function Index() {
         setHints([...hints, `Hint ${hints.length + 1}`]);
     };
 
-    let str = "dog";
-    let letters = str.split("");
+    const handleRandomWord = () => {
+        setUserInput("");
+        setHintIndex(0);
+        const url = `static/corpus.csv?wordType=${wordType}&cefrLevel=${cefrLevel}`;
+        fetch(url)
+            .then((response) => response.text())
+            .then((data) => {
+                const lines = data.split("\n");
+                const filteredLines = lines.filter((line) => {
+                    const fields = line.split(",");
+                    return (
+                        activatedWordTypes[wordTypes.indexOf(fields[1])] &&
+                        activatedCefrLevels[cefrLevels.indexOf(fields[2])]
+                    );
+                });
+                if (filteredLines.length > 0) {
+                    const randomIndex = Math.floor(Math.random() * filteredLines.length);
+                    const fields = filteredLines[randomIndex].split(",");
+                    setWord(fields[0].trim().replace('"', ""));
+                } else {
+                    setWord("No words found");
+                }
+            });
+    };
 
     const handleRevealNextLetter = () => {
         const input = document.querySelector(".input");
-        if (!letters.isNull && letters.length > 0) {
-            const letter = letters.shift();
-            input.value += letter;
+        const letters = word.split("");
+        input.value = "";
+        if (letters.length > hintIndex){
+            for (let i=0; i<hintIndex+1; i++){
+                input.value += letters[i];
+            }
+            setHintIndex(hintIndex +1);
+            console.log(input.value);
+            setUserInput(input.value);
         } else {
-            input.value = "";
-            letters = str.split("");
+            setHintIndex(0);
         }
     };
 
@@ -47,27 +96,71 @@ function Index() {
         <div className="container">
             <div className="word-types">
                 {wordTypes.map((type) => (
-                    <button key={type} onClick={() => handleWordTypeChange(type)} className="btn">
+                    <button
+                        key={type}
+                        onClick={() => handleWordTypeChange(type)}
+                        className={`btn ${activatedWordTypes[wordTypes.indexOf(type)] ? "active" : ""}`}
+                        style={{
+                            backgroundColor: activatedWordTypes[wordTypes.indexOf(type)] ? "#000000" : "#FFFFFF",
+                            color: activatedWordTypes[wordTypes.indexOf(type)] ? "#FFFFFF" : "#000000"
+                        }}
+                    >
                         {type}
                     </button>
                 ))}
             </div>
+            <div className="cefr-levels">
+                {cefrLevels.map((level) => (
+                    <button
+                        key={level}
+                        onClick={() => handleCefrLevelChange(level)}
+                        className={`btn ${
+                            activatedCefrLevels[cefrLevels.indexOf(level)] ? "active" : ""
+                        }`}
+                        style={{
+                            backgroundColor: activatedCefrLevels[cefrLevels.indexOf(level)] ? "#000000" : "#FFFFFF",
+                            color: activatedCefrLevels[cefrLevels.indexOf(level)] ? "#FFFFFF" : "#000000"
+                        }}
+                    >
+                        {level}
+                    </button>
+                ))}
+            </div>
             <div className="input-container">
-                <input type="text" value={word} onChange={handleWordChange} className="input" autocomplete="new-password" />
+                <input
+                    type="text"
+                    value={userInput}
+                    onChange={handleWordChange}
+                    className="input"
+                    autoComplete="off"
+                />
+                <button
+                    onClick={handleRandomWord}
+                    className="btn"
+                    disabled={hintIndex < word.length}
+                >
+                    Random Word
+                </button>
             </div>
             <div>
-                <button onClick={handleRevealNextLetter} className="btn" disabled={letters.length===0}>Reveal Next Letter</button>
+                <button
+                    onClick={handleRevealNextLetter}
+                    className="btn"
+                    disabled={hintIndex >= word.length}
+                >
+                    Reveal Next Letter
+                </button>
             </div>
             <div className="hints-container">
                 {hints.map((hint) => (
                     <p key={hint}>{hint}</p>
                 ))}
-                <button onClick={handleAddHint} className="btn">Add Hint</button>
+                <button onClick={handleAddHint} className="btn">
+                    Add Hint
+                </button>
             </div>
         </div>
     );
 }
 
 export default Index;
-
-
